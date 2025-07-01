5530fb59cb4c9230faaf02b6e3d2226a2f3775c0
Kaustubh-3110 <kaustubhbhoir31102007@gmail.com.com>
2025-07-01 13:08:48 +0530

    first commit

---
+++ HTML/project/testjs/1.test.js
@@ -0,0 +1,9 @@
+// function addition(value1,value2){
+//     const total = value1 + value2
+//     console.log('addition of values', total)
+// }
+
+// addition(10,10)
+
+const data= { name: kaustubh}
+console.log('data'data)
