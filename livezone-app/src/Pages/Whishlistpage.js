17246011e9f95f7706e13b17a4aae8ffa22fd448
Kaustubh-3110 <kaustubhbhoir31102007@gmail.com.com>
2025-08-17 21:05:17 +0530

    feat: enhance Whishlistpage to display product details with improved layout and styling

--- livezone-app/src/Pages/Whishlistpage.js
+++ livezone-app/src/Pages/Whishlistpage.js
@@ -1,11 +1,22 @@
-export default function Whishlistpage({likedProducts}) {
-    return (
-        <>
-            <div className="h-[90vh] ">
-              {likedProducts.map(item =>(
-                <p>{item.name}</p>
-              ))}  
+export default function Whishlistpage({ likesproducts }) {
+  const { products } = likesproducts;
+  return (
+    <>
+      <div className="h-[90vh]">
+        {products.map((product, index) => (
+          <div key={index} className="flex items-center p-3 justify-between border mx-2 my-2   hover:bg-gray-100 hover:shadow-lg gap-2">
+            <div className="flex items-center gap-2">
+              <p>{index + 1}.</p>
+              <BiSolidPurchaseTagAlt />
+              <p className="text-[16px]">{order.name}</p>
+              <p className="text-[14px]">({order.address})</p>
+              <p className="text-[12px]">{order.phone}</p>
             </div>
-        </>
-    );
-}
+            <div>
+              <p>₹{order.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );}tyiu
